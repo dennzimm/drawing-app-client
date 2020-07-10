0cd825fd44a3af8ab240895f65d19bce19efd932
Dennis Zimmermann <dennzimm@icloud.com>
2020-07-10 17:19:47 +0200

    Add styled canvas to PaperCanvas component

--- src/components/PaperCanvas/PaperCanvas.tsx
+++ src/components/PaperCanvas/PaperCanvas.tsx
@@ -1,22 +1,42 @@
 import { nanoid } from 'nanoid';
-import React from 'react';
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
 import { usePaper } from '../../hooks/usePaper.hook';
-import './PaperCanvas.css';
+import { useStoreState } from '../../store/hooks';
 
 interface PaperCanvasProps {}
 
 const CANVAS_ID = `paper-canvas-${nanoid()}`;
 
-const PaperCanvas: React.FC<PaperCanvasProps> = () => {
-  usePaper(CANVAS_ID);
+const Canvas = styled.canvas`
+  width: 100%;
+  height: 100%;
+  background: var(--paper-background-primary);
+  overflow: hidden;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0; 
+`;
 
-  return (
-    <canvas
-      id={CANVAS_ID}
-      data-paper-resize="true"
-      style={{ width: '100%', height: '100%' }}
-    ></canvas>
-  );
+const PaperCanvas: React.FC<PaperCanvasProps> = () => {
+  const { currentTool } = useStoreState((state) => state.tool);
+  const { isReady, updateFullViewSize } = usePaper(CANVAS_ID);
+
+  useEffect(() => {
+    if (isReady) {
+      updateFullViewSize();
+    }
+  }, [isReady, updateFullViewSize]);
+
+  useEffect(() => {
+    if (isReady) {
+      currentTool.tool.activate();
+    }
+  }, [currentTool.tool, isReady]);
+
+  return <Canvas id={CANVAS_ID} data-paper-resize="true" />;
 };
 
 export default PaperCanvas;
