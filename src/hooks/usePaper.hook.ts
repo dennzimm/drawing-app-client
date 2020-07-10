949309f13b927bc187970097361f0ff06dfd65cf
Dennis Zimmermann <dennzimm@icloud.com>
2020-07-10 17:21:02 +0200

    Update paper hook ready state

--- src/hooks/usePaper.hook.ts
+++ src/hooks/usePaper.hook.ts
@@ -1,12 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import paperProvider from '../providers/paper.provider';
 
 export function usePaper(id: string) {
+  const [isReady, setIsReady] = useState(false);
+
+  function updateFullViewSize() {
+    paperProvider.scope.view.viewSize = new paperProvider.scope.Size(
+      window.innerWidth,
+      window.innerHeight
+    );
+  }
+
   useEffect(() => {
     paperProvider.setup(id);
+    setIsReady(true);
 
     return () => {
       paperProvider.cleanup();
+      setIsReady(false);
     };
   }, [id]);
+
+  return {
+    updateFullViewSize,
+    isReady,
+  };
 }
