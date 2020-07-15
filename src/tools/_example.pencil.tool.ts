6219b3911a16699e3e30948aa29ff97db6c24c05
Dennis Zimmermann <dennzimm@icloud.com>
2020-07-15 17:20:55 +0200

    Comment out example pencil tool code

--- src/tools/_example.pencil.tool.ts
+++ src/tools/_example.pencil.tool.ts
@@ -1,131 +1,133 @@
-import {
-  AddItemEvent,
-  AddSegmentEvent,
-  emitOnView,
-} from '../helper/event.helper';
-import { addRoundLinecap } from '../helper/linecap.helper';
-import { createGroup, createPath } from '../helper/project.helper';
-import store from '../store';
-import { Tool } from './tool.abstract';
+// import {
+//   AddItemEvent,
+//   AddSegmentEvent,
+//   emitOnView,
+// } from '../helper/event.helper';
+// import { addRoundLinecap } from '../helper/linecap.helper';
+// import { createGroup, createPath } from '../helper/project.helper';
+// import store from '../store';
+// import { Tool } from './tool.abstract';
 
-class PencilTool extends Tool {
-  private layer: Nullable<paper.Layer> = null;
-  private group: Nullable<paper.Group> = null;
-  private path: Nullable<paper.Path> = null;
+// class PencilTool extends Tool {
+//   private layer: Nullable<paper.Layer> = null;
+//   private group: Nullable<paper.Group> = null;
+//   private path: Nullable<paper.Path> = null;
 
-  onMouseDown(event: paper.ToolEvent) {
-    // this.layer = createLayer();
+//   onMouseDown(event: paper.ToolEvent) {
+//     // this.layer = createLayer();
 
-    this.newPath();
-    this.addPoint(event.point);
+//     this.newPath();
+//     this.addPoint(event.point);
 
-    // this.setNewGroup();
+//     // this.setNewGroup();
 
-    this.addLinecap(event.point);
+//     this.addLinecap(event.point);
 
-    // this.layer.addChild(this.group!);
+//     // this.layer.addChild(this.group!);
 
-    // this.emitExportLayer();
-  }
+//     // this.emitExportLayer();
+//   }
 
-  onMouseDrag(event: paper.ToolEvent) {
-    if (this.path) {
-      this.addPoint(event.point);
-    }
-  }
+//   onMouseDrag(event: paper.ToolEvent) {
+//     if (this.path) {
+//       this.addPoint(event.point);
+//     }
+//   }
 
-  onMouseUp(event: paper.ToolEvent) {
-    if (this.path) {
-      this.addPoint(event.point);
-      this.addLinecap(event.point);
-      this.simplify();
-      this.deselectAll();
+//   onMouseUp(event: paper.ToolEvent) {
+//     if (this.path) {
+//       this.addPoint(event.point);
+//       this.addLinecap(event.point);
+//       this.simplify();
+//       this.deselectAll();
 
-      // TODO: Add History
-      // const action = new DrawAction({
-      //   layer: path.layer.name,
-      //   tool: store.getters.tool,
-      //   points: path.segments.map((seg) => {
-      //     return {
-      //       x: seg._point._x,
-      //       y: seg._point._y,
-      //     };
-      //   }),
-      // });
+//       // TODO: Add History
+//       // const action = new DrawAction({
+//       //   layer: path.layer.name,
+//       //   tool: store.getters.tool,
+//       //   points: path.segments.map((seg) => {
+//       //     return {
+//       //       x: seg._point._x,
+//       //       y: seg._point._y,
+//       //     };
+//       //   }),
+//       // });
 
-      // history.add(action);
-    }
-  }
+//       // history.add(action);
+//     }
+//   }
 
-  private simplify(tolerance?: number) {
-    if (this.path) {
-      this.path.simplify(tolerance);
-    }
-  }
+//   private simplify(tolerance?: number) {
+//     if (this.path) {
+//       this.path.simplify(tolerance);
+//     }
+//   }
 
-  private newPath() {
-    const { paperColor, width } = store.getState().tool;
+//   private newPath() {
+//     const { paperColor, width } = store.getState().tool;
 
-    this.path = createPath({
-      options: {
-        strokeColor: paperColor,
-        strokeWidth: width,
-      },
-    });
+//     this.path = createPath({
+//       options: {
+//         strokeColor: paperColor,
+//         strokeWidth: width,
+//       },
+//     });
 
-    this.emitAddItem(this.path);
-  }
+//     this.emitAddItem(this.path);
+//   }
 
-  private addPoint(point: paper.Point) {
-    if (this.path) {
-      this.path.add(point);
-      this.emitAddSegment(this.path.lastSegment);
-    }
-  }
+//   private addPoint(point: paper.Point) {
+//     if (this.path) {
+//       this.path.add(point);
+//       this.emitAddSegment(this.path.lastSegment);
+//     }
+//   }
 
-  private setNewGroup() {
-    this.group = createGroup({
-      options: {
-        layer: this.layer,
-      },
-    });
+//   private setNewGroup() {
+//     this.group = createGroup({
+//       options: {
+//         layer: this.layer,
+//       },
+//     });
 
-    this.path && this.group.addChild(this.path);
-  }
+//     this.path && this.group.addChild(this.path);
+//   }
 
-  private addLinecap(point: paper.Point) {
-    const { paperColor, width } = store.getState().tool;
+//   private addLinecap(point: paper.Point) {
+//     const { paperColor, width } = store.getState().tool;
 
-    const linecap = addRoundLinecap({
-      point,
-      color: paperColor,
-      width,
-      // layer: this.layer,
-      // group: this.group,
-    });
+//     const linecap = addRoundLinecap({
+//       point,
+//       color: paperColor,
+//       width,
+//       // layer: this.layer,
+//       // group: this.group,
+//     });
 
-    this.emitAddItem(linecap);
-  }
+//     this.emitAddItem(linecap);
+//   }
 
-  private emitAddItem(item: paper.Item) {
-    const itemAsJson = item.exportJSON();
+//   private emitAddItem(item: paper.Item) {
+//     const itemAsJson = item.exportJSON();
 
-    emitOnView<AddItemEvent>('event:add-item', {
-      id: item.name,
-      drawing: '0',
-      data: itemAsJson,
-    });
-  }
+//     emitOnView<AddItemEvent>('event:add-item', {
+//       id: item.name,
+//       drawing: '0',
+//       data: itemAsJson,
+//     });
+//   }
 
-  private emitAddSegment(segment: paper.Segment) {
-    const segmentAsJson = JSON.stringify(segment);
+//   private emitAddSegment(segment: paper.Segment) {
+//     const segmentAsJson = JSON.stringify(segment);
 
-    emitOnView<AddSegmentEvent>('event:add-segment', {
-      id: this.path!.name,
-      drawing: '0',
-      data: segmentAsJson,
-    });
-  }
-}
+//     emitOnView<AddSegmentEvent>('event:add-segment', {
+//       id: this.path!.name,
+//       drawing: '0',
+//       data: segmentAsJson,
+//     });
+//   }
+// }
 
-export const pencilTool = new PencilTool();
+// export const pencilTool = new PencilTool();
+
+export default {};
