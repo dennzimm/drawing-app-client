9753c8eb6fafcd480e2c040ca5a7916c3bf8b14c
Dennis Zimmermann <dennzimm@icloud.com>
2020-08-07 20:46:43 +0200

    Add NewSegmentFragment

--- src/api/graphql/subscriptions/segment.subscriptions.ts
+++ src/api/graphql/subscriptions/segment.subscriptions.ts
@@ -1,23 +1,28 @@
 import { gql } from "@apollo/client";
 
+export const NewSegmentFragment = gql`
+  fragment NewSegmentData on NewSegment {
+    node {
+      layerID
+      groupID
+      itemID
+      strokeColor
+      fillColor
+      strokeWidth
+      segmentData {
+        x
+        y
+      }
+    }
+  }
+`;
+
 export const DRAWING_DATA_PUBLISHED = gql`
   subscription DrawingDataPublished($userID: ID!, $drawingID: ID!) {
     drawingDataPublished(userID: $userID, drawingID: $drawingID) {
       __typename
-      ... on NewSegment {
-        node {
-          layerID
-          groupID
-          itemID
-          strokeColor
-          fillColor
-          strokeWidth
-          segmentData {
-            x
-            y
-          }
-        }
-      }
+      ...NewSegmentData
     }
   }
+  ${NewSegmentFragment}
 `;
