b5c109246ccaf9a8bb7330994fcc713017853bbc
Dennis Zimmermann <dennzimm@icloud.com>
2020-08-07 20:40:04 +0200

    Refactor useDrawingCanvasSubscriptions hook

--- src/components/DrawingCanvas/hooks/useDrawingCanvasSubscriptions.hook.ts
+++ src/components/DrawingCanvas/hooks/useDrawingCanvasSubscriptions.hook.ts
@@ -1,25 +1,9 @@
-import { useSubscription } from "@apollo/client";
 import { useEffect } from "react";
-import {
-  DrawingDataPublished,
-  DrawingDataPublishedVariables,
-} from "../../../graphql/@types/gql-operations.types";
-import { DRAWING_DATA_PUBLISHED } from "../../../graphql/subscriptions/drawing.subscriptions";
-import { useStoreState } from "../../../store/hooks";
-import { handleSegmentAdded } from "../../../paper/helper";
+import { useDrawingDataPublished } from "../../../api/hooks";
+import { PaperDataHandlerFactory } from "../../../paper/factories";
 
 export function useDrawingCanvasSubscriptions() {
-  const userID = useStoreState((state) => state.user.userID);
-
-  const { data } = useSubscription<
-    DrawingDataPublished,
-    DrawingDataPublishedVariables
-  >(DRAWING_DATA_PUBLISHED, {
-    variables: {
-      drawingID: "1",
-      userID,
-    },
-  });
+  const { data } = useDrawingDataPublished();
 
   useEffect(() => {
     if (data) {
@@ -27,9 +11,8 @@ export function useDrawingCanvasSubscriptions() {
         drawingDataPublished: { __typename, node },
       } = data;
 
-      if (node) {
-        handleSegmentAdded(node);
-      }
+      const dataHandler = PaperDataHandlerFactory.build(__typename);
+      dataHandler && dataHandler(node);
     }
   }, [data]);
 }
