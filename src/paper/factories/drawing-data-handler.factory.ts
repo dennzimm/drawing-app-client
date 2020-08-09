0d075ec4f1493fac181c4461237c3d9aa80626bf
Dennis Zimmermann <dennzimm@icloud.com>
2020-08-09 15:44:12 +0200

    Update paper data hadnler factory

--- src/paper/factories/drawing-data-handler.factory.ts
+++ src/paper/factories/drawing-data-handler.factory.ts
@@ -1,12 +1,17 @@
-import { DrawingDataPublished_drawingDataPublished } from "../../api/@types/gql-operations.types";
+import {
+  DrawingDataPublished_drawingDataPublished,
+  GetDrawing_drawing_items,
+} from "../../api/@types/gql-operations.types";
 import { handleSegmentAdded } from "../helper";
 
-export type PaperDataTypes = DrawingDataPublished_drawingDataPublished["__typename"];
+export type PaperDataTypes =
+  | DrawingDataPublished_drawingDataPublished["__typename"]
+  | GetDrawing_drawing_items["__typename"];
 
 export class PaperDataHandlerFactory {
   public static build(typeName: PaperDataTypes) {
     switch (typeName) {
-      case "NewSegment":
+      case "SegmentAdded":
         return handleSegmentAdded;
 
       default:
