e82b0c5150ce9df1f24126d5647bc1ba8af62030
Dennis Zimmermann <dennzimm@icloud.com>
2020-08-10 20:21:20 +0200

    Update root files

--- apollo.config.js
+++ apollo.config.js
@@ -2,7 +2,7 @@ module.exports = {
   client: {
     service: {
       name: "drawing-app",
-      localSchemaFile: "./src/api/@types/gql.schema.json",
+      localSchemaFile: "./src/api/@types/generated/gql.schema.json",
     },
     includes: ["./src/**/*.ts"],
     excludes: ["**/__tests__/**", "**/node_modules/**", "**/dist/**"],
