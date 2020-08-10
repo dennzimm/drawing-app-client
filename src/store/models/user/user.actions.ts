6e26ebe889f24261b451004e6eb9a26081778892
Dennis Zimmermann <dennzimm@icloud.com>
2020-08-10 20:32:16 +0200

    Cleanup user store module

--- src/store/models/user/user.actions.ts
+++ src/store/models/user/user.actions.ts
@@ -5,23 +5,12 @@ export enum UserAction {
   setLoading = "setLoading",
   setUserData = "setUserData",
   setDarkMode = "setDarkMode",
-
-  // --- Thunks:
-  // setIsLoggedIn = 'setIsLoggedIn',
-  // setUsername = 'setUsername',
-  // setUserID = 'setUserID',
-  // setHasSeenTutorial = 'setHasSeenTutorial',
 }
 
 export interface UserActions {
   [UserAction.setLoading]: Action<UserModel, boolean>;
   [UserAction.setUserData]: Action<UserModel, Partial<UserState>>;
   [UserAction.setDarkMode]: Action<UserModel, boolean>;
-  // --- Thunks:
-  // [UserAction.setIsLoggedIn]: Action<UserModel, boolean>;
-  // [UserAction.setUsername]: Action<UserModel, string | undefined>;
-  // [UserAction.setUserID]: Action<UserModel, string>;
-  // [UserAction.setHasSeenTutorial]: Action<UserModel, boolean>;
 }
 
 const userActions: UserActions = {
@@ -37,19 +26,6 @@ const userActions: UserActions = {
       ...userData,
     };
   }),
-  // --- Thunks:
-  // [UserAction.setIsLoggedIn]: action((state, isLoggedIn) => {
-  //   state.isLoggedin = isLoggedIn;
-  // }),
-  // [UserAction.setUsername]: action((state, username) => {
-  //   state.username = username;
-  // }),
-  // [UserAction.setUserID]: action((state, userID) => {
-  //   state.userID = userID;
-  // }),
-  // [UserAction.setHasSeenTutorial]: action((state, hasSeenTutorial) => {
-  //   state.hasSeenTutorial = hasSeenTutorial;
-  // }),
 };
 
 export default userActions;
--- src/store/models/user/user.thunks.ts
+++ src/store/models/user/user.thunks.ts
@@ -1,18 +1,18 @@
-import { Thunk, thunk } from 'easy-peasy';
-// import {
-//   getUserData,
-//   setHasSeenTutorialData,
-//   setIsLoggedInData,
-//   setUsernameData,
-// } from '../../../services/storage.service';
-import { UserModel } from './user.model';
+import { Thunk, thunk } from "easy-peasy";
+import {
+  getUserData,
+  setHasSeenTutorialData,
+  setIsLoggedInData,
+  setUsernameData,
+} from "../../../services";
+import { UserModel } from "./user.model";
 
 export enum UserThunk {
-  loadUserData = 'loadUserData',
-  logoutUser = 'logoutUser',
-  setIsLoggedIn = 'setIsLoggedIn',
-  setUsername = 'setUsername',
-  setHasSeenTutorial = 'setHasSeenTutorial',
+  loadUserData = "loadUserData",
+  logoutUser = "logoutUser",
+  setIsLoggedIn = "setIsLoggedIn",
+  setUsername = "setUsername",
+  setHasSeenTutorial = "setHasSeenTutorial",
 }
 
 export interface UserThunks {
@@ -26,25 +26,25 @@ export interface UserThunks {
 const userThunks: UserThunks = {
   [UserThunk.loadUserData]: thunk(async (actions) => {
     actions.setLoading(true);
-    // const userData = await getUserData();
-    // actions.setUserData(userData);
+    const userData = await getUserData();
+    actions.setUserData(userData);
     actions.setLoading(false);
   }),
   [UserThunk.logoutUser]: thunk(async (_, __, { getState }) => {
-    // await setIsLoggedInData(false);
+    await setIsLoggedInData(false);
     getState().username = undefined;
   }),
   [UserThunk.setIsLoggedIn]: thunk(async (_, loggedIn, { getState }) => {
-    // await setIsLoggedInData(loggedIn);
+    await setIsLoggedInData(loggedIn);
     getState().isLoggedin = loggedIn;
   }),
   [UserThunk.setUsername]: thunk(async (_, username, { getState }) => {
-    // await setUsernameData(username);
+    await setUsernameData(username);
     getState().username = username;
   }),
   [UserThunk.setHasSeenTutorial]: thunk(
     async (_, hasSeenTutorial, { getState }) => {
-      // await setHasSeenTutorialData(hasSeenTutorial);
+      await setHasSeenTutorialData(hasSeenTutorial);
       getState().hasSeenTutorial = hasSeenTutorial;
     }
   ),
