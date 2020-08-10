e7fe5ed74ec0f9cbae1f1db0bea63a1142d8d5a6
Dennis Zimmermann <dennzimm@icloud.com>
2020-08-10 20:21:44 +0200

    Remove global helper and config

--- src/helper/server-status.helper.ts
+++
@@ -1,9 +0,0 @@
-import { NetworkStatusType } from "../store/models/app/app.model";
-import store from "../store";
-
-export function setServerConnectionStatus(serverStatus: NetworkStatusType) {
-  const setServerConnectionStatusAction = store.getActions().app
-    .setServerConnectionStatus;
-
-  setServerConnectionStatusAction(serverStatus);
-}
