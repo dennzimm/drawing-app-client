a2c2d57c6b5d64a4bdcb4e8a7bbe194bde9c5f2d
Dennis Zimmermann <dennzimm@icloud.com>
2020-08-24 17:28:21 +0200

    Update page exports

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,3 +3,4 @@ export { DrawingsSelect } from "./DrawingsSelect";
 export { Login } from "./Login";
 export { Signup } from "./Signup";
 export { Account } from "./Account";
+export { UnderConstruction } from "./UnderConstruction";
