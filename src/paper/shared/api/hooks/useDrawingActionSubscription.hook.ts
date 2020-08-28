a3c866cc2ea28317d78a7cdc728520679538c64b
Dennis Zimmermann <dennzimm@icloud.com>
2020-08-28 11:29:09 +0200

    Add subscribed method & drawingName prop to subscription hooks

--- src/paper/shared/api/hooks/useDrawingActionSubscription.hook.ts
+++ src/paper/shared/api/hooks/useDrawingActionSubscription.hook.ts
@@ -1,57 +1,65 @@
 import { useApolloClient } from "@apollo/client";
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 import {
   DrawingActionPublished,
   DrawingActionPublishedVariables,
 } from "../../../../api/@types/generated/gql-operations.types";
 import { DRAWING_ACTION_PUBLISHED } from "../../../../api/graphql/drawing-action.graphql";
+import { DEBUG } from "../../../../constants";
 import { useStoreState } from "../../../../store/hooks";
 import { paperDrawingApiImportService } from "../services/paper-drawing-api-import.service";
 
 export function useDrawingActionSubscription() {
   const userId = useStoreState((state) => state.user.userID);
-  const drawingName = useStoreState((state) => state.drawing.id);
 
   const client = useApolloClient();
 
   const subscription = useRef<ZenObservable.Subscription>();
   const [data, setData] = useState<DrawingActionPublished>();
 
-  const subscribe = useCallback(() => {
-    const observer = client.subscribe<
-      DrawingActionPublished,
-      DrawingActionPublishedVariables
-    >({
-      query: DRAWING_ACTION_PUBLISHED.subscription,
-      variables: {
-        userId,
-        drawingName,
-      },
-    });
+  const subscribe = useCallback(
+    (drawingName: string) => {
+      const observer = client.subscribe<
+        DrawingActionPublished,
+        DrawingActionPublishedVariables
+      >({
+        query: DRAWING_ACTION_PUBLISHED.subscription,
+        variables: {
+          userId,
+          drawingName,
+        },
+      });
 
-    subscription.current = observer.subscribe(({ data }) => {
-      console.log("DRAWING_ACTION_PUBLISHED received", data);
+      subscription.current = observer.subscribe(({ data }) => {
+        if (data) {
+          const {
+            drawingActionPublished: { action, node },
+          } = data;
 
-      if (data) {
-        const {
-          drawingActionPublished: { action, node },
-        } = data;
+          setData(data);
+          paperDrawingApiImportService.importDrawingActionData(action, node);
+        }
+      });
 
-        setData(data);
-        paperDrawingApiImportService.importDrawingActionData(action, node);
-      }
-    });
-
-    console.log("subscribed DRAWING_ACTION_PUBLISHED");
-  }, [client, drawingName, userId]);
+      DEBUG && console.log("subscribed DRAWING_ACTION_PUBLISHED");
+    },
+    [client, userId]
+  );
 
   const unsubscribe = useCallback(() => {
     subscription.current && subscription.current.unsubscribe();
-    console.log("unsubscribed DRAWING_ACTION_PUBLISHED");
+
+    DEBUG && console.log("unsubscribed DRAWING_ACTION_PUBLISHED");
   }, []);
+
+  const subscribed = useMemo(
+    () => subscription.current && !subscription.current.closed,
+    []
+  );
 
   return {
     data,
+    subscribed,
     subscribe,
     unsubscribe,
   };
--- src/paper/shared/api/hooks/useItemMutationSubscription.hook.ts
+++ src/paper/shared/api/hooks/useItemMutationSubscription.hook.ts
@@ -1,55 +1,61 @@
 import { useApolloClient } from "@apollo/client";
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 import {
   ItemMutated,
   ItemMutatedVariables,
 } from "../../../../api/@types/generated/gql-operations.types";
 import { ITEM_MUTATED } from "../../../../api/graphql/item.graphql";
+import { DEBUG } from "../../../../constants";
 import { useStoreState } from "../../../../store/hooks";
 import { paperDrawingApiItemService } from "../services/paper-drawing-api-item.service";
 
 export function useItemMutationSubscription() {
   const userId = useStoreState((state) => state.user.userID);
-  const drawingName = useStoreState((state) => state.drawing.id);
 
   const client = useApolloClient();
 
   const subscription = useRef<ZenObservable.Subscription>();
   const [data, setData] = useState<ItemMutated>();
 
-  const subscribe = useCallback(() => {
-    const observer = client.subscribe<ItemMutated, ItemMutatedVariables>({
-      query: ITEM_MUTATED.subscription,
-      variables: {
-        userId,
-        drawingName,
-      },
-    });
+  const subscribe = useCallback(
+    (drawingName: string) => {
+      const observer = client.subscribe<ItemMutated, ItemMutatedVariables>({
+        query: ITEM_MUTATED.subscription,
+        variables: {
+          userId,
+          drawingName,
+        },
+      });
 
-    subscription.current = observer.subscribe(({ data }) => {
-      console.log("ITEM_MUTATED received", data);
+      subscription.current = observer.subscribe(({ data }) => {
+        if (data) {
+          const {
+            itemMutated: { mutation, node },
+          } = data;
+          setData(data);
+          paperDrawingApiItemService.itemMutation(mutation, node);
+        }
+      });
 
-      if (data) {
-        const {
-          itemMutated: { mutation, node },
-        } = data;
-        console.log("useItemMutationSubscription -> data", data);
-
-        setData(data);
-        paperDrawingApiItemService.itemMutation(mutation, node);
-      }
-    });
-
-    console.log("subscribed ITEM_MUTATED");
-  }, [client, drawingName, userId]);
+      DEBUG && console.log("subscribed ITEM_MUTATED");
+    },
+    [client, userId]
+  );
 
   const unsubscribe = useCallback(() => {
     subscription.current && subscription.current.unsubscribe();
-    console.log("unsubscribed ITEM_MUTATED");
+
+    DEBUG && console.log("unsubscribed ITEM_MUTATED");
   }, []);
+
+  const subscribed = useMemo(
+    () => subscription.current && !subscription.current.closed,
+    []
+  );
 
   return {
     data,
+    subscribed,
     subscribe,
     unsubscribe,
   };
