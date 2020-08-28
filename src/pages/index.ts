b1780665123e86dc99d79f0f9bed02b6fd7b7cab
Dennis Zimmermann <dennzimm@icloud.com>
2020-08-28 16:14:36 +0200

    Add tutorial page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,4 +3,5 @@ export { Drawing } from "./Drawing";
 export { DrawingsSelect } from "./DrawingsSelect";
 export { Login } from "./Login";
 export { Signup } from "./Signup";
+export { Tutorial } from "./Tutorial";
 export { UnderConstruction } from "./UnderConstruction";
