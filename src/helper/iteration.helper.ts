3747185065554149af7250b73dafd45c5a3913a3
Dennis Zimmermann <dennzimm@icloud.com>
2020-10-30 15:22:02 +0100

    Update documentation for iteration helper

--- src/helper/iteration.helper.ts
+++ src/helper/iteration.helper.ts
@@ -1,5 +1,18 @@
+/**
+ * waitFor
+ * https://codeburst.io/javascript-async-await-with-foreach-b6ba62bbf404
+ *
+ * @param ms
+ */
 export const waitFor = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
+/**
+ * asyncForEach
+ * https://codeburst.io/javascript-async-await-with-foreach-b6ba62bbf404
+ *
+ * @param array
+ * @param callback
+ */
 export async function asyncForEach<T = unknown, R = unknown>(
   array: T[],
   callback: (current: T, index: number, values: T[]) => Promise<R>
