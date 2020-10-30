0d4abaa7df5eb2311d65fb58a731fb137469eede
Dennis Zimmermann <dennzimm@icloud.com>
2020-10-30 18:01:35 +0100

    Update paper hooks documentation

--- src/paper/hooks/usePaperEvent.hook.ts
+++ src/paper/hooks/usePaperEvent.hook.ts
@@ -4,6 +4,22 @@ import { fromEvent, Subscription } from "rxjs";
 import { useStoreState } from "../../store/hooks";
 import { PaperViewEvents } from "../@types";
 
+/**
+ * usePaperEvent
+ *
+ * This hook can be used to link a specific PaperViewEvent
+ * to a callback function (but only when a paper setup
+ * (setupPaper() -> usePaper hook) has been executed).
+ *
+ * If the passed event is emitted on the paper view,
+ * the callback function is executed.
+ *
+ * @export
+ * @template P
+ * @param {PaperViewEvents} event
+ * @param {(payload: P) => void} callback
+ * @return {unsubscribeEvent} A function to cancel the event subscription.
+ */
 export function usePaperEvent<P>(
   event: PaperViewEvents,
   callback: (payload: P) => void
